84affa64220f9448d07cda13f982a919257376ba
Diego <juan.luque@colegiomacaya.cl>
2023-05-29 15:45:13 -0400

    conf docker and env

--- app/core/config/config.py
+++ app/core/config/config.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path=env_path)
 class Settings:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DB_MONGO = os.environ.get('DB_MONGO2')
-    RUTA_CLUSTER=os.environ.get('RUTA_MONGO2')
+    RUTA_CLUSTER=os.environ.get('RUTA_CLUSTER')
     ACCESS_TOKEN_EXPIRE_MINUTES = 60
     ALGORITHM = os.environ.get('ALGORITHM')
     ORIGINS_MAIN = os.environ.get('ORIGINS_MAIN')
