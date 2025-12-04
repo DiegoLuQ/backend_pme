20818a38fd2ffa1e47703f07cbdb90f87a36cc93
root <root@pme.mcdp.cl>
2025-12-04 18:20:12 +0000

    modificaciones

--- app/core/db/repo_recursos.py
+++ app/core/db/repo_recursos.py
@@ -65,7 +65,6 @@ def obtener_recursos(id_pme: str, year: int):
 def obtener_actividades_por_pme(id_pme):
     try:
         result = coleccion_recursos.find({'id_pme': id_pme}, {
-            '_id': 0,
             'id_pme': 0,
             'fecha': 0,
             'monto': 0
